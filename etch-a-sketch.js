00057056f5e81909ab888805dc4935601eead177
UC917 <3298331419@qq.com>
2025-09-20 10:15:51 +0800

    feat(js文件): 生成颜色
    让颜色随机生成而不是固定的

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -25,3 +25,11 @@ function createGrid(size) {
         container.appendChild(gridItem);
     }
 }
+
+// 生成随机颜色
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
